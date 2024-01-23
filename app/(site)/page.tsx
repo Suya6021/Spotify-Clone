457e6a2e568c8af61be0e72182460788b609cd3a
Suya6021 <smangaj@jspmrscoe.edu.in>
2024-01-23 21:00:31 +0530

    Basic:initalize and components

---
+++ app/(site)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Index = () => {
+  return (
+    <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
+
+      
+    </div>
+  )
+}
+
+export default Index
